e8915dd27a5147d41d714f0405d07bf5699c099c
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-03-05 09:10:56 +0200

    adding some changes

--- src/data/people.js
+++ src/data/people.js
@@ -1,21 +1,21 @@
 const people = [
   {
     id: 1,
-    name: "Alice Johnson",
+    name: "Henrich UMUGWANEZA",
     role: "Software Engineer",
     department: "Engineering",
     avatar: "https://api.dicebear.com/8.x/lorelei/svg?seed=Tobias&backgroundColor=c8e6f9",
   },
   {
     id: 2,
-    name: "Michael Smith",
-    role: "UI/UX Designer",
+    name: "Fabrice ASSA",
+    role: "UI/UX Developer",
     department: "Design",
     avatar: "https://api.dicebear.com/8.x/lorelei/svg?seed=Marcus&backgroundColor=d1f4cc",
   },
   {
     id: 3,
-    name: "Sarah Williams",
+    name: "Arthur Henry",
     role: "Project Manager",
     department: "Management",
     avatar: "https://api.dicebear.com/8.x/lorelei/svg?seed=Amara&backgroundColor=b6e3f4",
